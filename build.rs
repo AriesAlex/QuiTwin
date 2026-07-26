e322911c29439769aaa6bee0d5123bce63021873
AriesAlex <famliamarchenko@yandex.ru>
2026-07-27 01:31:36 +0300

    chore: credit ArieX as project author

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
             .set_icon("assets/quitwin.ico")
             .set("ProductName", "QuiTwin")
             .set("FileDescription", "QuiTwin Discord and Equicord launcher")
-            .set("LegalCopyright", "Copyright (c) 2026 QuiTwin contributors");
+            .set("LegalCopyright", "Copyright (c) 2026 ArieX");
         resource
             .compile()
             .expect("failed to compile Windows resources");
